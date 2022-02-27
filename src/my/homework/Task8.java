b64c8b921a4085f225229d3c8d0efa29aed3935b
ArtyomKhodas <a.Khodas1601@gmail.com>
2022-02-27 13:02:15 +0300

    Output on the screen the number with the word "Рублей" in the correct case.

---
+++ src/my/homework/Task8.java
@@ -0,0 +1,27 @@
+package my.homework;
+
+public class Task8 {
+    public static void main(String[] args) {
+        int x = 51;
+        if(x % 10 == 1) {
+            System.out.println(x + " рубль" );
+        } else if ( x % 10 == 2) {
+            System.out.println(x + " рубля");
+        } else if ( x % 10 == 3) {
+            System.out.println(x + " рубля");
+        } else if ( x % 10 == 4) {
+            System.out.println(x + " рубля");
+        } else if ( x % 10 == 5) {
+            System.out.println(x + "рублей");
+        } else if ( x % 10 == 6) {
+            System.out.println(x + " рублей");
+        } else if ( x % 10 == 7) {
+            System.out.println(x + " рублей");
+        } else if ( x % 10 == 8) {
+            System.out.println(x + "рублей");
+        } else if ( x % 10 == 9) {
+            System.out.println(x + "рублей");
+        } else;
+
+    }
+}
