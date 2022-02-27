b2ace6e1b3c4566c167e5a492762ec03d2c6bd7b
ArtyomKhodas <a.Khodas1601@gmail.com>
2022-02-27 12:56:55 +0300

    check if the round cardboard can hide the rectangular hole

---
+++ src/my/homework/Task7.java
@@ -0,0 +1,16 @@
+package my.homework;
+
+public class Task7 {
+    public static void main(String[] args) {
+        int a = 2, b = 3;
+        int r = 8;
+        if(r * r >= (a * a) + ( b * b)/4 ) {
+            System.out.println("Прямоугольное отверстие закроется");
+        } else {
+            System.out.println("Прямоугольное отверстие не закроется, нужен радиус окружности побольше.");
+
+        }
+
+
+    }
+}
