8e26b79754ac649d596470a412956156182145f2
ArtyomKhodas <a.Khodas1601@gmail.com>
2022-02-27 12:38:50 +0300

    The values of the variables

---
+++ src/my/homework/Task3.java
@@ -0,0 +1,32 @@
+package my.homework;
+
+public class Task3 {
+    public static void main(String[] args) {
+        byte b = 0x55;
+        System.out.println(b);
+
+        short s = 0x55ff;
+        System.out.println(s);
+
+        int i = 1000_000;
+        System.out.println(i);
+
+        long l = 0xffffffffL;
+
+        char c = 'a';
+        System.out.println(c);
+
+        float f = .25f;
+        System.out.println(f);
+
+        double d = .00001234;
+        System.out.println(d);
+
+        boolean bool = true;
+        System.out.println(bool);
+        System.out.println();
+
+
+    }
+}
+
