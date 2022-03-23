7bf2af4d8274bdfc0162c8f3eb5db2babc46e397
ArtyomKhodas <a.Khodas1601@gmail.com>
2022-03-23 19:49:42 +0300

    first commit. With problems :^(

---
+++ src/my/homework/TimeInterval/Main.java
@@ -0,0 +1,18 @@
+package my.homework.TimeInterval;
+
+public class Main {
+    public static void main(String[] args) {
+        Time time = new Time(1,30,00);
+        time.outputTime();
+        time.printReturnSec();
+        Time time1 = new Time(5400);
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/my/homework/TimeInterval/Time.java
@@ -0,0 +1,54 @@
+package my.homework.TimeInterval;
+
+public class Time {
+    private int second;
+    private int minute;
+    private int hour;
+
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public Time(int totalSec) {
+        this.hour =totalSec;
+        this.second = totalSec;
+        this.minute = totalSec;
+
+    }
+
+    public void outputTime() {
+        if((hour <= 23) && (hour >=0) &&((minute<=59) && (minute >=0) &&(second<=59) && (second >=0))) {
+            System.out.println("Заданное время: "  + hour + ":" + minute + ":" + second);
+        } else System.out.println("Error!");
+    }
+
+    public int returnSec() {
+
+        System.out.println("Общее количество секунд:");
+
+        return (this.hour * 3600) + (this.minute * 60) + this.second;
+    }
+    public void printReturnSec() {
+        System.out.println(returnSec());
+
+    }
+    public void compareTime() {
+
+    }
+
+}
