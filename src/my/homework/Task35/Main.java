2d5cea3b9e8e7f800dd1b4cfb0c06565f19a8034
ArtyomKhodas <a.Khodas1601@gmail.com>
2022-04-07 23:36:49 +0300

    Using "for each"

--- src/my/homework/Task35/Main.java
+++ src/my/homework/Task35/Main.java
@@ -4,11 +4,11 @@ package my.homework.Task35;
 
 public class Main {
     public static void main(String[] args) {
-        Season season1 = Season.SPRING;
-        Season season2 = Season.SUMMER;
-        Season season3 = Season.AUTUMN;
-        Season season4 = Season.WINTER;
+        for(Season seasons: Season.values()) {
+            System.out.println(seasons);
+        }
 
-        System.out.println(season1 + " , " + season2 + " , " + season3 + " , " + season4 + "." );
+
+
     }
 }
