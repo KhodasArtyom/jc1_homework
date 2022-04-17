6c620a57a83c6eaaaf140a24a681f5c0b628a0f1
ArtyomKhodas <a.Khodas1601@gmail.com>
2022-04-17 13:11:46 +0300

    updates

--- src/my/homework/TimeInterval/Main.java
+++ src/my/homework/TimeInterval/Main.java
@@ -2,10 +2,11 @@ package my.homework.TimeInterval;
 
 public class Main {
     public static void main(String[] args) {
-        Time time = new Time(1,30,00);
+        Time time = new Time(1,31,00);
         time.outputTime();
         time.printReturnSec();
         Time time1 = new Time(5400);
+
 
 
 
--- src/my/homework/TimeInterval/Time.java
+++ src/my/homework/TimeInterval/Time.java
@@ -4,12 +4,17 @@ public class Time {
     private int second;
     private int minute;
     private int hour;
+    private int totalSec;
 
 
     public Time(int hour, int minute, int second) {
         this.hour = hour;
         this.minute = minute;
         this.second = second;
+    }
+
+    public Time(int totalSec) {
+
     }
 
     public int getHour() {
@@ -24,12 +29,7 @@ public class Time {
         return second;
     }
 
-    public Time(int totalSec) {
-        this.hour =totalSec;
-        this.second = totalSec;
-        this.minute = totalSec;
 
-    }
 
     public void outputTime() {
         if((hour <= 23) && (hour >=0) &&((minute<=59) && (minute >=0) &&(second<=59) && (second >=0))) {
