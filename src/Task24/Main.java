ea54eca62274f7def288870f0fde9e020587df17
ArtyomKhodas <a.Khodas1601@gmail.com>
2022-04-18 19:53:12 +0300

    TimeInterval

---
+++ src/Task24/Main.java
@@ -0,0 +1,13 @@
+package Task24;
+
+public class Main {
+    // Как получилось и как я понимаю эту задачу.
+    public static void main(String[] args) {
+        TimeInterval object1 = new TimeInterval(3600);
+        TimeInterval object2 = new TimeInterval(1,0,00);
+        object1.printAllSeconds();
+        object2.printDividedTime();
+        object2.compareTime();
+
+    }
+}
---
+++ src/Task24/TimeInterval.java
@@ -0,0 +1,42 @@
+package Task24;
+
+public class TimeInterval {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public TimeInterval(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public TimeInterval(int allseconds) {
+        second = allseconds;
+    }
+
+    public int returnAllSeconds() {
+        return (hour * 3600) + (minute * 60) + second;
+
+    }
+
+    public void printAllSeconds() {
+        System.out.println("Общее количество секунд в Object № 1 = " + second);
+    }
+
+    public void printDividedTime() {
+        System.out.println(String.format("Object2 = %s hours, %s minutes, %s seconds", hour, minute,second));
+    }
+
+    public int compareTime() {
+        if(this.second >= this.returnAllSeconds()){
+            System.out.println("Object1 >= Object2");
+        } else {
+            System.out.println("Object1 < Object2");
+        }
+        return this.second - this.returnAllSeconds();
+
+    }
+
+
+}
